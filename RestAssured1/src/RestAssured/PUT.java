50be04bc9a80e053c719867aa5dc7fa0f2df9873
Harsha <Harsha@DESKTOP-R5AVHAA>
2022-06-22 22:54:48 +0530

    send the code

---
+++ RestAssured1/src/RestAssured/PUT.java
@@ -0,0 +1,30 @@
+package RestAssured;
+
+import org.json.simple.JSONObject;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class PUT 
+{
+
+	public static void main(String[] args) 
+	{
+		RestAssured.baseURI="https://reqres.in/";
+		RequestSpecification a = RestAssured.given();
+		JSONObject obj= new JSONObject();
+		obj.put("name", "morpheus");
+		obj.put("dob", "leader");
+		a.contentType(ContentType.JSON);
+		a.body(obj.toJSONString());
+		Response b = a.request(Method.PUT,"api/users/2");
+		System.out.println(b.asString());
+		System.out.println(b.statusCode());
+		System.out.println(b.statusLine());
+
+	}
+
+}
